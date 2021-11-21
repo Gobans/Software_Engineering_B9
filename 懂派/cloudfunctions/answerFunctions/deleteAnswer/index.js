d8fbb78f69df316a24409b5ce6b8d31389fd1278
Gobans <audghks4452@gmail.com>
2021-11-21 14:18:14 +0800

    ans question cloud functions

---
+++ 懂派/cloudfunctions/answerFunctions/deleteAnswer/index.js
@@ -0,0 +1,14 @@
+const cloud = require('wx-server-sdk')
+const db = cloud.database()
+const _ = db.command
+
+exports.main = async (event, context) => {
+  try {
+    return await db.collection('answer').where({
+      _id: event.answer_id,
+      user_id: event.user_id
+    }).remove()
+  } catch(e) {
+    console.error(e)
+  }
+}
