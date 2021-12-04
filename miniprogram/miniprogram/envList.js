f889d14de9b4a69025cd0131501d5f91738f1aff
zhaosh <zhaosh18@mails.tsinghua.edu.cn>
2021-12-04 13:39:03 +0800

    修改了登录状态与查看个人提问

--- miniprogram/miniprogram/envList.js
+++
@@ -1,6 +0,0 @@
-const envList = [{"envId":"cloud1-5gum0luvfc692c31","alias":"cloud1"},{"envId":"test-2goxdq5n98cf5e1e","alias":"test"}]
-const isMac = true
-module.exports = {
-    envList,
-    isMac
-}
