66a638b6304f735fa17992ca07dd435dcf120fdf
zhaosh <zhaosh18@mails.tsinghua.edu.cn>
2021-12-04 17:34:23 +0800

    增加查看个人回答与删除回答功能

---
+++ Software_Engineering_B9/懂派/uploadCloudFunction.bat
@@ -0,0 +1,1 @@
+"D:\Tencent\微信web开发者工具\cli.bat" cloud functions deploy --e cloud1-5gum0luvfc692c31 --n quickstartFunctions --r --project "C:\Users\Administrator\WeChatProjects\miniprogram-3" --report_first && "D:\Tencent\微信web开发者工具\cli.bat" cloud functions deploy --e test-2goxdq5n98cf5e1e --n quickstartFunctions --r --project "C:\Users\Administrator\WeChatProjects\miniprogram-3" --report
